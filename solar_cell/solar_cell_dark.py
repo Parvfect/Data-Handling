cb0633d454219cb5b4094ad14ad98a3a21af0c3b
Parvfect <parvagrw02@gmail.com>
2021-11-18 17:52:43 +0000

    Okay

--- solar_cell/solar_cell_dark.py
+++ solar_cell/solar_cell_dark.py
@@ -4,21 +4,31 @@ import matplotlib.pyplot as plt
 from curve_fitting import curve_fitting, objective
 import math
 
-q = 1.602176634e-20
-T = 300
+q = 1.602176634e-19
+T = 298
 
 def get_boltzmann(a, b):
     return -(q*b)/(a*T*2.54) 
 
-df = pd.read_excel("solarcell.xlsx")
+df = pd.read_excel("scd.xlsx")
 
-print(df["I1"])
+print(df["I2"])
 
+x = df["V2"][2:]
+y = df["I2"][2:]
+
+print(y)
+y = [math.log(i) for i in y]
+
+plt.plot(x,y)
+plt.show()
+"""
 x = df["V1"]
 y = df["I1"]
 
 y = y[5:10]
 x = x[5:10]
+
 
 y = [math.log(i) for i in y]
 y1, a, b = curve_fitting(x, y)
@@ -31,3 +41,4 @@ plt.errorbar(x,y,xerr=0.01, yerr=0.01, fmt='o')
 plt.title("Steffan Boltzmann Law with k as {}".format(get_boltzmann(a, b)))
 plt.show()
 
+"""
