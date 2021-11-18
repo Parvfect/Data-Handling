3395b800772f62cda1589aaf11cdde9f21032d51
Parvfect <parvagrw02@gmail.com>
2021-11-18 21:27:28 +0000

    Some good work here folks

--- solar_cell/solar_cell_dark.py
+++ solar_cell/solar_cell_dark.py
@@ -6,6 +6,7 @@ import math
 
 q = 1.602176634e-19
 T = 298
+kb = 1.38e-23
 
 def get_boltzmann(a, b):
     return -(q*b)/(a*T*2.54) 
@@ -17,20 +18,9 @@ print(df["I2"])
 x = df["V2"][2:]
 y = df["I2"][2:]
 
-print(y)
-y = [math.log(i) for i in y]
-
-plt.plot(x,y)
-plt.show()
-"""
-x = df["V1"]
-y = df["I1"]
-
-y = y[5:10]
-x = x[5:10]
-
 
 y = [math.log(i) for i in y]
+
 y1, a, b = curve_fitting(x, y)
 
 
@@ -38,7 +28,8 @@ plt.plot(x,y, 'o')
 plt.plot(x, y1)
 plt.xlabel("Voltage (V) +/- 0.01 V")
 plt.errorbar(x,y,xerr=0.01, yerr=0.01, fmt='o')
-plt.title("Steffan Boltzmann Law with k as {}".format(get_boltzmann(a, b)))
+plt.title("ln(I) vs V")
 plt.show()
 
-"""
+print("Saturation Current - {}".format(math.exp(-b)/100))
+print("Ideality Factor - {}".format((-q * b)/(kb* T * a)/100))
